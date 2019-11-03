2ffccdb30b5456ae4fb70c776972e13db009c8b6
zavolokas <sergey.zavoloka@gmail.com>
2019-11-03 11:04:50 +0000

    initial commit

---
+++ InpaintHTTP/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace InpaintHTTP
+{
+    using System.IO;
+    using Microsoft.AspNetCore.Hosting;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+               .UseContentRoot(Directory.GetCurrentDirectory())
+               .UseKestrel()
+               .UseStartup<Startup>()
+               .UseUrls("http://localhost:8069")
+               .Build();
+
+            host.Run();
+        }
+    }
+}
